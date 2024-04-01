e4c039d4793a98856a64e9bbbdaab8b83cd2dfbd
knowaboutayan <info.creationology@gmail.com>
2024-04-02 04:48:09 +0530

    footer content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="border-2  w-full  bottom-0 bg-green-900 py-4 text-gray-300">
       <div className="container mx-auto text-center">
-        <p>&copy; 2024 Your Company. All rights reserved.</p>
+        <p>&copy; 2024 creationology. All rights reserved.</p>
       </div>
     </footer>
   );
