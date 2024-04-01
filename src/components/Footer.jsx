25bc1651c4d0b9a1c1ed82f839a8866529f92bec
knowaboutayan <info.creationology@gmail.com>
2024-04-02 04:46:49 +0530

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="border-2 fixed border-red-600 w-full  bottom-0 bg-green-900 py-4 text-gray-300">
+    <footer className="border-2  w-full  bottom-0 bg-green-900 py-4 text-gray-300">
       <div className="container mx-auto text-center">
         <p>&copy; 2024 Your Company. All rights reserved.</p>
       </div>
